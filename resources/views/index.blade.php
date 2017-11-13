04262b5faa5a52718833c9f70b121131763198e8
gscsdlz <lz842063523@foxmail.com>
2017-11-13 14:58:54 +0800

    完成首页轮播

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,4 +3,41 @@
     Welcome To NUC Online Judge
 @endsection
 @section('main')
+    <div style="margin-top:-80px" id="carousel" class="carousel slide" data-ride="carousel" data-pause="false"  data-interval="60000">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel" data-slide-to="1"></li>
+            <li data-target="#carousel" data-slide-to="2"></li>
+        </ol>
+        <script>
+            /*$(document).ready(function(){
+                var height = window.screen.height;
+                var width = window.screen.width;
+                $("#background").css('height', height);
+                $("#background").css('width', width);
+            })*/
+        </script>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src="{{ URL('image/1.jpg') }}" alt="...">
+                <div class="carousel-caption">
+                    <h1><a href="{{ URL('video/1.mp4') }}">Everybody should learn how to program</a></h1>
+                </div>
+            </div>
+            <div class="item">
+                <img src="{{ URL('image/2.jpg') }}" alt="...">
+                <div class="carousel-caption">
+                    <h1>了解什么是ACM-ICPC吧！</h1>
+                </div>
+            </div>
+            <div class="item">
+                <img src="{{ URL('image/3.jpg') }}" alt="...">
+                <div class="carousel-caption">
+                    <h1>快来加入ACM-Team吧！</h1>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
